d58f9f0d2247db794ea35b20386e85b86b0cb220
Kevin Pastor <kevinpastor.mtl@gmail.com>
2026-07-18 23:04:10 -0400

    feat: remake landing page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,6 @@ import {
     createRootRoute,
     HeadContent,
     Scripts,
-    Link,
 } from '@tanstack/react-router'
 
 import css from './-index.css?url'
@@ -14,16 +13,8 @@ const RootDocument = ({ children }: PropsWithChildren): ReactNode => (
         <head>
             <HeadContent />
         </head>
-        <body className="h-full ">
-            <header className="bg-red-500 sticky top-0">
-                <div className="flex justify-between items-center gap-2 p-2 bg-green-500 max-w-7xl mx-auto">
-                    <h1 className="text-3xl font-bold">Keb's Keebs</h1>
-                    <Link to='/docs/$'>Docs</Link>
-                </div>
-            </header>
-            <main className="p-2 max-w-7xl mx-auto">
-                {children}
-            </main>
+        <body className="h-full">
+            {children}
             <Scripts />
         </body>
     </html>
