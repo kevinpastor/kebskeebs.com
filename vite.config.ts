118228e43ac1e64fe47ab48bc44bf46954476d80
Kevin Pastor <kevinpastor.mtl@gmail.com>
2026-07-18 23:08:06 -0400

    feat: setup cloudflare wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,19 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import babel from '@rolldown/plugin-babel'
 import contentCollections from "@content-collections/vite";
 import tailwindcss from '@tailwindcss/vite'
-
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
     plugins: [
+        cloudflare({ viteEnvironment: { name: "ssr" } }),
         tanstackStart({
             router: {
                 quoteStyle: "double"
+            },
+            prerender: {
+                enabled: true
             }
         }),
-        // tanstackRouter({
-        //     target: 'react',
-        //     autoCodeSplitting: true,
-        //     quoteStyle: "double",
-        // }),
         react(),
         tailwindcss(),
         babel({ presets: [reactCompilerPreset()] }),
